f4ee1d4a6b4b0fd4836e8986a3efc548ad881a85
cx <cxandmjq@gmail.com>
2014-10-13 20:28:40 +0800

    completed

--- matric_multiplication.c
+++ matric_multiplication.c
@@ -17,32 +17,46 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
-void matric_multiply(int* a,int n,int *b,int m, int *res);
+void matric_multiply(int* a,int n,int* b,int m,int length_b, int *res);
 int main()
 {
     int i,j;
-    int a[2][1] = {2,1};
-    int b[1][2] = {3,3};
-    int *res = (int*)malloc(2*sizeof(int));
-    matric_multiply(a,2,b,1, res);
-    for(i = 0;i<2;i++)
-    {
-        for(j = 0;j<1;j++)
-            printf("%d\n",*(res+j+i));
-    }
+    int a[4][3] = {1,0,4,
+                   2,1,1,
+                   3,1,0,
+                   0,2,2};
+    int b[3][2] = {2,4,
+                    1,1,
+                    3,0};
+    int *res = (int*)malloc(8*sizeof(int));
+    matric_multiply(a,4,b,3,6,res);
+    for(i = 0;i<8;i++)
+            printf("%d\n",*(res+i));
+    free(res);
+    return 0;
 }
-void matric_multiply(int* a,int n,int *b,int m, int *res)
+void matric_multiply(int* a,int n,int* b,int m,int length_b, int *res)
 {
-    int h = 0;
-    for(;h<m*n;h++)
-        *(res+h) = 0;
-    int k = sizeof(*a)/(n*sizeof(int));
-    int i,j,x;
-    for(i = 0; i < n ; n++)
-        for(j = 0;j < m; j++)
-            for(m = 0; x < k; x++)
-            {
-                *(res+i+j) = *(res+i+j)+*(a+i+j)*(*(b+i+j));
-            }
+    //自己的
+//    int h = 0;
+      int k = length_b/m;
+      printf("%d\n",k);
+//    for(;h<m*k;h++)
+//        *(res+h) = 0;
+//      int i,j,x;
+//    for(i = 0; i < n ; n++)
+//        for(j = 0;j < k; j++)
+//            for(x = 0; x < m; x++)
+//                *(res+i*k+j) = *(res+i*k+j) + *(a+i*m+x)*(*(b+k*x+j));
+//
+//  书上
+    int i,j,q;
+    for(i=0; i<n; i++)
+        for(j=0; j<k; j++)
+        {
+            *(res+i*k+j) = 0;
+            for(q=0; q<m; q++)
+                *(res+i*k+j) = *(res+i*k+j) + *(a+i*m+q)*(*(b+q*k+j));
+        }
 }
 
